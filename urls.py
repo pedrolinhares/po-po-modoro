1fa3f0dce9e68c1f0d1166caafa72689bc9785a4
Pedro Henrique Linhares <pedrolmota@gmail.com>
2011-11-29 22:23:20 -0200

    Add user to app tasks and create login form

--- urls.py
+++ urls.py
@@ -4,6 +4,10 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('apps',
-	(r'^$', 'home_page.views.show_index'),
+urlpatterns = patterns('',
+	(r'^$', 'apps.home_page.views.show_index'),
+	(r'^login/$', 'django.contrib.auth.views.login', 
+				 {'template_name': 'login.html'}),
+	(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
+				  {'login_url': '/login/'}),
 )

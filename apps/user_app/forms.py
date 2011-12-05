a7ba6ece76e768e642a6ed264791e3987f7c7629
Pedro Henrique Linhares <pedrolmota@gmail.com>
2011-12-04 22:44:25 -0200

    Implement validation to the username field.

--- apps/user_app/forms.py
+++ apps/user_app/forms.py
@@ -1,23 +1,25 @@
 from django import forms
-from django.core import validators
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+def isValidUserName(username):
+	try:
+		User.objects.get(username=username)
+	except User.DoesNotExist:
+		return
+	raise ValidationError('The username "%s" is already taken.' % username)
+
 
 class RegistrationForm(UserCreationForm):
 
 	username = forms.CharField(label='username',
 							max_length=30,
-							required=True,) #validators=[self.isValidUserName])		
+							required=True, validators=[isValidUserName])		
 	class Meta:
 		model = User 
 		fields = ('username','first_name', 'last_name', 'email',)
 
-#	def isValidUserName(self, field_data, all_data):
-#		try:
-#			User.objects.get(username=field_data)
-#		except User.DoesNotExist:
-#			return
-#		raise validators.ValidationError('The username "%s" is already taken.' % field_data)
 
 	def save(self, commit=True):
 		new_user = super(RegistrationForm, self).save(commit=False)
@@ -28,3 +30,4 @@ class RegistrationForm(UserCreationForm):
 			new_user.save()
 
 		return new_user
+

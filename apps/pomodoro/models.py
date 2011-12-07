f1b73f57a3d63a913af654affcf241ff8cd6e84f
Pedro Henrique Linhares <pedrolmota@gmail.com>
2011-12-07 16:23:59 -0200

    Remove worksheet tuple from models.py

--- apps/pomodoro/models.py
+++ apps/pomodoro/models.py
@@ -4,12 +4,8 @@ from apps.user_app.models import UserProfile
 class Task(models.Model):
 	"""Tasks are units of work measured in terms of
 	   pomororos."""
-	WORKSHEETS = (
-			('0', 'none'),
-			('1', 'inventory'),
-			('2', 'todo'))
 	title = models.CharField(max_length=100)
-	num_pomodoros = models.IntegerField()
-	worksheet = models.CharField(max_length=100, choices=WORKSHEETS)
+	pomodoros = models.IntegerField()
+	worksheet = models.CharField(max_length=50)
 	user = models.ForeignKey(UserProfile)
 

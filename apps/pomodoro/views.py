e4635125834d5f892b8850717d95fc0bb57c4387
Pedro Henrique Linhares <pedrolmota@gmail.com>
2011-12-07 15:13:37 -0200

    inventory e todo-today now require login to be accessed

--- apps/pomodoro/views.py
+++ apps/pomodoro/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 from apps.pomodoro.models import Task
 
 def dashboard(request):
 	return render_to_response('dashboard.html')
 
+@login_required
 def todo_today(request):
 	return render_to_response('todo-today.html')
 
+@login_required
 def inventory(request):
 	tasks_in_inventory = Task.objects.filter(worksheet="inventory")
 	return render_to_response('inventory.html', {'tasks_in_inventory': tasks_in_inventory})
